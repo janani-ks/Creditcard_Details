53ce3cbbbfa7800f30bb63461219c07e62043df1
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-01-31 15:24:46 +0530

    Add files via upload

---
+++ Creditcard/src/main/java/credit/Creditcard.java
@@ -0,0 +1,27 @@
+package credit;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+class Details implements Cloneable{
+    String name;
+    String num;
+    String exdate;
+    Details(String n,String n1,String e){
+        this.name=n;
+        this.num=n1;
+        this.exdate=e;
+    }
+    public Object clone()throws CloneNotSupportedException{  
+		return super.clone();  
+		}  
+}
+public class Creditcard
+{
+	public static void main(String[] args) throws CloneNotSupportedException{
+		Logger l = Logger.getLogger("com.api.jar");
+		Details c = new Details("JANANI","35464246562","08-01-2024");
+		Details c1 = new Details("KOKILA","7385657657","17-03-2023");
+		Details c3 = (Details)c.clone();
+		l.log(Level.INFO,()-> ""+(c.num).equals(c1.num));
+		l.log(Level.INFO,()->"Credit card Name : "+c3.name+"\nCredit card Number : "+c3.num+"\nExpired Date : "+c3.exdate);
+	}
+}
